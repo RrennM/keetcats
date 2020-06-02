807d503639b3737c87d3f6d5eaf22b72d9224431
Tyson Hood <TysonHood@gmail.com>
2020-06-02 14:54:18 -0700

    Setup menu-item & directory components, and styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet"></link>
   </React.StrictMode>,
   document.getElementById('root')
 );
